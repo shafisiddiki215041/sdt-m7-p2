c2b64f97637255207e722889df792165588ed51c
shafisiddiki215041 <140194303+shafisiddiki215041@users.noreply.github.com>
2024-06-19 15:56:56 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,78 +1,102 @@
-// const allBox = document.getElementsByClassName("box-1");
+const DisplaySearchItem = () => {
 
-// for(let i=0; i<allBox.length; i++){
-//     const ele = allBox[i];
-//     ele.style.backgroundColor = "green";
-//     if(ele.innerText == "box-5"){
-//         ele.style.backgroundColor = "red";
-//     }
-// }
-// const  allTag  = document.getElementsByTagName("div");
+    const input = document.getElementById("catch-input").value;
 
-// for (let i = 0; i < allTag.length; i++) {
-//   allTag[i].style.backgroundColor = "red";
-// } 
+    const firstcontainer = document.getElementById("first-open");
+    firstcontainer.innerHTML = '';
 
-
-
-document.getElementById("handleButton").addEventListener("click", (event)=>{
-    const inputvalue = document.getElementById("text-box").value; 
-    console.log(inputvalue);
-
-    const comment = document.getElementById("comment-container");
-    
-
-    const p = document.createElement("p");
-    p.classList.add("child");
-    p.innerText = inputvalue;
-
-    comment.appendChild(p); 
-
-    document.getElementById("text-box").value = "" ; 
-
-    const  allcomments = document.getElementsByClassName("child");
-
-
-    for(const ele of allcomments){
-        ele.addEventListener("click", (e) =>{
-            e.target.parentNode.removeChild(ele);
+    console.log(input);
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            displayProduct(data.meals);
         })
-    }
-   
-}) 
+        .catch((err) => {
+            Details(0);
 
+            const second = document.getElementById("second-container");
+            
+            second.innerHTML="";
 
+            const div = document.createElement("div");
 
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(res => res.json())
-.then(data =>{
-    console.log(data);
-    displayData(data);
-})
-.catch((err)=>{
-    console.log(err);
-})
+            div.innerHTML=`
+            <h1 class="h1-tag">NOT FOUND</h1>
+            `;
 
+            second.appendChild(div);
 
-const displayData = (userData)=>{
-    const container = document.getElementById("userData-container");
+        })
 
-    userData.forEach(user => {
-        console.log(user);
+    // console.log(input);
+}
+
+const displayProduct = (meals) => {
+    const mealContainer = document.getElementById("meal-container");
+
+    mealContainer.innerHTML="";
+
+    meals.forEach(meal => {
+        // console.log(meal);
         const div = document.createElement("div");
 
-        div.classList.add("user");
+        div.classList.add("card1");
 
         div.innerHTML = `
-        <h4> Title</h4>
-        <p>Description</p>
-        <button>Details</button>
+        <img class="card-img" onclick="Details(${meal.idMeal})" src="${meal.strMealThumb}" alt="" />
+        <h5 class=" mt-5 p-3">${meal.strMeal}</h5>
         `;
-
-        container.appendChild(div);
-
+        mealContainer.appendChild(div);
     });
 }
-// const handleclick = (event) =>{
-//     console.log("Hello Boxxxx");
-// }
+
+const Details = (mealId) => {
+
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            const firstcontainer = document.getElementById("first-open");
+
+             firstcontainer.innerHTML = '';
+
+            const div = document.createElement("div");
+
+            div.classList.add("card");
+
+            div.innerHTML = `
+            <img src="${data.meals[0].strMealThumb}" class="card-img-top card-img2" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${data.meals[0].strMeal}</h5>
+            <h6 class="mt-2"></h6>
+            <ul id="ingredient-list"></ul>
+            `;
+
+            firstcontainer.appendChild(div);
+
+
+             for (let i = 1; i <= 20; i++) {
+                const ingredient = data.meals[0][`strIngredient${i}`];
+                const measure = data.meals[0][`strMeasure${i}`];
+
+                if (ingredient) {
+                    const ingredientItem = document.createElement("li");
+                    ingredientItem.textContent = `${ingredient} - ${measure}`;
+                    document.getElementById("ingredient-list").appendChild(ingredientItem);
+                } else {
+                    break;
+                }
+            }
+
+            firstcontainer.scrollIntoView({ behavior: "smooth", block: "start" });
+
+        })
+        .catch(error => {
+            console.log("NO DETAILS");
+        });
+}
+
+// DisplaySearchItem();
